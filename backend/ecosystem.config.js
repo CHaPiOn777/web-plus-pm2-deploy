2fa28ba2da94c4d1d8480139505aa02f21c6c757
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-07-11 16:38:35 +0300

    2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -5,21 +5,6 @@ const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY = 'origin/master',
 } = process.env;
 
-// export const apps = [{
-//   name: "mesto",
-//   script: "dist/app.js"
-// }];
-// export const deploy = {
-//   production: {
-//     user: DEPLOY_USER,
-//     host: DEPLOY_HOST,
-//     ref: DEPLOY_REF,
-//     repo: DEPLOY_REPOSITORY,
-//     path: DEPLOY_PATH,
-//     'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-//     'post-deploy': 'cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-//   },
-// };
 
 module.exports = {
   apps: [{
