4fa69ee65316d3305e7edb2f5ef85010b2f1c9da
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-07-11 16:31:21 +0300

    2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,42 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env.deploy" });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY = 'origin/master',
+} = process.env;
+
+// export const apps = [{
+//   name: "mesto",
+//   script: "dist/app.js"
+// }];
+// export const deploy = {
+//   production: {
+//     user: DEPLOY_USER,
+//     host: DEPLOY_HOST,
+//     ref: DEPLOY_REF,
+//     repo: DEPLOY_REPOSITORY,
+//     path: DEPLOY_PATH,
+//     'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+//     'post-deploy': 'cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+//   },
+// };
+
 module.exports = {
-  apps : [{
-    name   : "app1",
-    script : "./app.js"
-  }]
-}
+  apps: [{
+    name: "mesto",
+    script: "dist/app.js"
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
